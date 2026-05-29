4675cdf03014f9d9339cdc5309697e73a877df2d
AndersonAugsten <anderson_volnei@hotmail.com>
2026-05-29 10:55:43 -0300

    Add files via upload

--- nerd-do-campo/src/index.js
+++ nerd-do-campo/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AdminAppCompleto } from './Admin';
+
+const isAdmin = window.location.pathname.startsWith('/admin');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    {isAdmin ? <AdminAppCompleto /> : <App />}
+  </React.StrictMode>
+);
